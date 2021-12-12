88de0dbfd269ef02fa494e44c5b125f98593e4a1
Wesley Yee <wesleyyee1996@gmail.com>
2021-12-12 18:04:54 +0000

    updated udp comms

--- udp_communication/scripts/udp_comms.py
+++ udp_communication/scripts/udp_comms.py
@@ -2,9 +2,10 @@
 
 import rospy
 import socket
+import thread
 
 from geometry_msgs.msg import Twist
-from serial_communication.msg import SensorData
+from geometry_msgs.msg import TwistStamped
 from std_msgs.msg import String
 
 UDP_IP = ""
@@ -22,12 +23,12 @@ robo_id = GetRobotId()
 socket_send_loc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket_send_loc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-def send_robot_location_callback(sensor_data):
+def send_robot_location_callback(vive_data):
     """
     every time we receive sensor data from the serial_communication
     ROS node, we package it up along with the robot ID and broadcast
     """
-    to_send = str(robo_id.robot_id) + "," + str(sensor_data.x1) + "," + str(sensor_data.y1)
+    to_send = str(robo_id.robot_id) + "," + str(vive_data.twist.linear.x) + "," + str(vive_data.twist.linear.y)
 
     #rospy.loginfo("Sending robot location "+str(to_send))
     socket_send_loc.sendto(to_send.encode(), ("192.168.0.255", 2510))
@@ -51,6 +52,25 @@ def get_local_ip():
     rospy.loginfo("Connected to local IP %s ", s.getsockname()[0])
     s.close()
 
+
+
+def get_can_locations():
+    can_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("192.168.0.255", 1510))
+    
+    can_pub = rospy.Publisher('can_location', Twist, queue_size=10)
+
+    while not rospy.is_shutdown():
+        try:
+            can_data, addr = can_socket.recvfrom(1024)
+            can_data = data.split(',')
+            can_msg = Twist()
+            can_msg.linear.x = float(can_data[1])
+            can_msg.linear.y = float(can_data[2])
+            can_pub.publish(can_msg)
+        except socket.timeout:
+            rospy.logerror("Can data timed out! Reconnecting")
+
 def udp_comms():
 
     # set up publisher to motor_control topic
@@ -60,7 +80,7 @@ def udp_comms():
     rospy.Subscriber("robot_id", String, get_robot_id)
 
     # set up subscriber callback function for sensor_data topic
-    rospy.Subscriber("sensor_data", SensorData, send_robot_location_callback)
+    rospy.Subscriber("vive_data", TwistStamped, send_robot_location_callback)
     
     # initialize the udp_communication node
     rospy.init_node('udp_communication', anonymous=True)
